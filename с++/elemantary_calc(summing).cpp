29764449e52b98c17e272a97fb21bb165e366dcf
Илья Усов <usov.is@phystech.edu>
2023-03-15 01:16:38 +0300

    added

---
+++ с++/elemantary_calc(summing).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout << "введите два числа" << "\n";
+    cin >> a >> b;
+    cout << "сумма равна: " << a + b << "\n";
+
+    return 0;
+}
